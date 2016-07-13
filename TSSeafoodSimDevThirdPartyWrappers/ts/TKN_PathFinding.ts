307150728201f257ac2a79a69e2ea83ec6446041
Seregfaroth <bl@tokni.com>
2016-07-13 15:02:20 +0100

    pathfinding wrapper

---
+++ TSSeafoodSimDevThirdPartyWrappers/ts/TKN_PathFinding.ts
@@ -0,0 +1,21 @@
+﻿
+
+class TKN_PathFinding {
+    private m_finder;
+    private m_navGrid;
+
+    constructor() {
+        this.m_finder = new PF.AStarFinder();
+        //this.m_navGrid = PF.Grid(navTable);
+    }
+    findPath(xOrigin, yOrigin, xDest, yDest): number[][] {
+        var ret: number[][];
+        var backup = this.m_navGrid.clone();
+        ret = this.m_finder.findPath(xOrigin, yOrigin, xDest, yDest, this.m_navGrid);
+        this.m_navGrid = backup;
+        return ret;        
+    }
+    set navTable(p_nav: number[][]) {
+        this.m_navGrid = new PF.Grid(p_nav);
+    }
+}
